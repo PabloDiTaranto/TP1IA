f86394ee3125f5b07bca5bf8d8360ac0b84bdd17
camilacandidoo <64676265+camilacandidoo@users.noreply.github.com>
2021-06-24 01:07:02 -0300

    Update DistanceWeaponGOAP.cs

--- IA2/Assets/00_IA2/GOAP/FSM/States/DistanceWeaponGOAP.cs
+++ IA2/Assets/00_IA2/GOAP/FSM/States/DistanceWeaponGOAP.cs
@@ -5,16 +5,30 @@ using FSM;
 
 public class DistanceWeaponGOAP : MonoBaseState
 {
+    private EnemyGOAPController _enemyGOAPController;
+    private bool executeOnce;
     private void Awake()
     {
+        _enemyGOAPController = FindObjectOfType<EnemyGOAPController>();
     }
 
     public override void UpdateLoop()
     {
+        if (!executeOnce)
+        {
+            _enemyGOAPController.DistanceWeapon.SetActive(true);
+            _enemyGOAPController._hasMeleeWeapon = false;
+            _enemyGOAPController._hasDistanceWeapon = true;
+            executeOnce = true;
+        }
     }
 
     public override IState ProcessInput()
     {
+        executeOnce = false;
+        if (_enemyGOAPController._hasDistanceWeapon && Transitions.ContainsKey("OnDistanceAttackGOAP"))
+            return Transitions["OnDistanceAttackGOAP"];
+
         return this;
     }
 }
